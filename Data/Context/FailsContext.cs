1ef0de8e0b848556ca6431cab943826d0e524df4
Sebastian Pino <pino.sebastian16@gmail.com>
2020-12-29 17:38:26 -0500

    DBContext

--- Data/Context/FailsContext.cs
+++ Data/Context/FailsContext.cs
@@ -1,7 +1,11 @@
-namespace Practica_04.Data.Context
+using Practica_04.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Practica_04.Data
 {
-    public class FailsContext
+    public class FailsContext : DbContext
     {
-        
+        public FailsContext(DbContextOptions dco) : base(dco) {
+
+        }
     }
 }
